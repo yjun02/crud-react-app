38fbbe3b73473c8010d48f82f9524238a15b5a5a
yjun02 <jyjoons@gmail.com>
2024-11-18 17:28:58 +0900

    using Outlet

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import "./App.css";
 import CreateUser from "./components/User/CreateUser";
 import ShowUser from "./components/User/ShowUser";
-import { Route, Routes } from "react-router-dom";
+import { Router, Route, Routes } from "react-router-dom";
 import EditUser from "./components/User/EditUser";
 import User from "./components/User/User";
 import Header from "./components/Common/Header";
 import Home from "./components/Layout/Home";
 function App() {
-  return (
-    <div className="App">
-      <header className="container">
-        <div className="">
-          <Header />
-          <Routes>
-          
-            <Route path="/" element={<Home />} />
-            <Route path="/edit-user/:id" element={<EditUser />} />
-            <Route path="/user/:id" element={<User />} />
-            <Route path="/create-user" element={<CreateUser />} />
-            <Route path="/show-user" element={<ShowUser />} />
-          </Routes>
-          
+    return (
+        <div className="App">
+            <header className="container">
+                <div className="">
+                    <Routes>
+                        <Route path="/" element={<Header />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/edit-user/:id" element={<EditUser />} />
+                            <Route path="/user/:id" element={<User />} />
+                            <Route path="/create-user" element={<CreateUser />} />
+                            <Route path="/show-user" element={<ShowUser />} />
+                        </Route>
+                    </Routes>
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
